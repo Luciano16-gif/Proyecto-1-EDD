3500a60beed51bcf34b8c1912c59ea53dc2a9e24
Luciano 16-gif <chanominardo10@gmail.com>
2024-10-14 15:49:39 -0400

    Cambiado el codigo para usar JFileChooser

--- src/Objetos/Funcion.java
+++ src/Objetos/Funcion.java
@@ -1,24 +1,22 @@
 package Objetos;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.File;
+import java.io.FileReader;
+import javax.swing.JFileChooser;
 import primitivas.Lista;
 import org.json.JSONObject;
 
 /**
- * Esta clase define las funciones referentes al JSON. 
- * 
- * @author Gabriele Colarusso, Luciano Minardo, Ricardo Paez
- * 
+ * Esta clase define las funciones referentes al JSON.
+ *
+ * @author
  * @version: 13/10/2024
  */
 public class Funcion {
 
-    public static void ReadJsonMetro(String args) {
-        String urlString = args;
-        Lista<Estacion> estaciones = readJson(urlString);
+    public static void ReadJsonMetro() {
+        Lista<Estacion> estaciones = readJson();
 
         Grafos grafo = new Grafos();
 
@@ -33,38 +31,36 @@ public class Funcion {
         grafo.mostrarGrafo();
     }
 
-    public static Lista<Estacion> readJson(String urlString) {
+    public static Lista<Estacion> readJson() {
         Lista<Estacion> estaciones = new Lista<>();
 
         try {
-            // Crear la URL y abrir la conexión
-            URL url = new URL(urlString);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
+            // Abrir un JFileChooser para seleccionar el archivo JSON
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setDialogTitle("Seleccione el archivo JSON de la red de transporte");
 
-            // Verificar la respuesta
-            int responseCode = conn.getResponseCode();
-            if (responseCode != 200) {
-                System.out.println("Error al conectar: Código de respuesta " + responseCode);
-                return estaciones;
+            int userSelection = fileChooser.showOpenDialog(null);
+
+            if (userSelection == JFileChooser.APPROVE_OPTION) {
+                File jsonFile = fileChooser.getSelectedFile();
+
+                // Leer el contenido del archivo JSON
+                BufferedReader in = new BufferedReader(new FileReader(jsonFile));
+                StringBuilder content = new StringBuilder();
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                in.close();
+
+                // Convertir el contenido en un objeto JSON
+                JSONObject jsonObject = new JSONObject(content.toString());
+
+                // Llamar a la función para extraer estaciones
+                extractEstaciones(jsonObject, estaciones);
+            } else {
+                System.out.println("No se seleccionó ningún archivo.");
             }
-
-            // Leer el contenido
-            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            StringBuilder content = new StringBuilder();
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            in.close();
-            conn.disconnect();
-
-            // Convertir el contenido en un objeto JSON
-            JSONObject jsonObject = new JSONObject(content.toString());
-
-            // Llamar a la función para extraer estaciones
-            extractEstaciones(jsonObject, estaciones);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -79,13 +75,16 @@ public class Funcion {
         for (String sistema : jsonObject.keySet()) {
             Object sistemaObj = jsonObject.get(sistema);
 
-            if (sistemaObj instanceof Iterable) {
-                for (Object lineaObj : (Iterable<?>) sistemaObj) {
-                    JSONObject lineaJson = (JSONObject) lineaObj;
-                    for (String linea : lineaJson.keySet()) {
-                        Object estacionesObj = lineaJson.get(linea);
-                        if (estacionesObj instanceof Iterable) {
-                            for (Object estacion : (Iterable<?>) estacionesObj) {
+            if (sistemaObj instanceof org.json.JSONArray) {
+                org.json.JSONArray lineasArray = (org.json.JSONArray) sistemaObj;
+                for (int i = 0; i < lineasArray.length(); i++) {
+                    JSONObject lineaObj = lineasArray.getJSONObject(i);
+                    for (String linea : lineaObj.keySet()) {
+                        Object estacionesObj = lineaObj.get(linea);
+                        if (estacionesObj instanceof org.json.JSONArray) {
+                            org.json.JSONArray estacionesArray = (org.json.JSONArray) estacionesObj;
+                            for (int j = 0; j < estacionesArray.length(); j++) {
+                                Object estacion = estacionesArray.get(j);
                                 if (estacion instanceof String) {
                                     estaciones.append(new Estacion((String) estacion, linea, sistema, lineaIndex));
                                 } else if (estacion instanceof JSONObject) {
@@ -96,7 +95,7 @@ public class Funcion {
                                 }
                             }
                         }
-                        lineaIndex++; // Incrementar el índice para la siguiente línea
+                        lineaIndex++;
                     }
                 }
             }
